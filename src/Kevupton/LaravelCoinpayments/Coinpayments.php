3419635e4102d40c1cb2216a9ec5a0f918237be6
janruls1 <janruls1@gmail.com>
2020-05-02 23:17:56 +0530

    Added success_url and cancel_url in transactions

--- src/Kevupton/LaravelCoinpayments/Coinpayments.php
+++ src/Kevupton/LaravelCoinpayments/Coinpayments.php
@@ -79,6 +79,7 @@ class Coinpayments
         $acceptableFields = [
             'address', 'buyer_email', 'buyer_name',
             'item_name', 'item_number', 'invoice', 'custom', 'ipn_url',
+            'success_url', 'cancel_url'
         ];
 
         $request = [
---
+++ src/database/migrations/2020_05_02_051156_update_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SetupCoinpaymentTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $prefix = cp_table_prefix();
+
+        Schema::table($prefix . 'transactions', static function (Blueprint $table) {
+            $table->string('success_url')->after('checkout_url');
+            $table->string('cancel_url')->after('checkout_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $prefix = cp_table_prefix();
+
+        Schema::table($prefix . 'transactions', static function (Blueprint $table) {
+            $table->dropColumn('success_url');
+            $table->dropColumn('cancel_url');
+        });
+    }
+}
